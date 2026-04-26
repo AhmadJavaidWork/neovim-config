af90bde8565d79e62a7b6c92fd2ad3fe21651dbd
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2026-04-26 15:56:49 +0500

    fixed telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,16 +2,25 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "v0.2.2",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-	opts = {
-		defaults = {
-			file_ignore_patterns = { "node_modules/", "%.git/", "compare_codebases/" },
-			get_status_text = function(self)
-				return ""
-			end,
-		},
-	},
 	config = function()
+		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
+
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = {
+					"^node_modules/",
+					"^.git/",
+					"^compare_codebases/",
+					"target/",
+					"dist/",
+				},
+				get_status_text = function(self)
+					return ""
+				end,
+			},
+		})
+
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>fa", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
