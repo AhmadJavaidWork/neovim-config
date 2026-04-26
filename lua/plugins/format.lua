e9ad314311235274b4a714095abc1b72447b3c70
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2026-04-26 13:59:03 +0500

    added typescript to format

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -5,6 +5,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				json = { "prettierd", "prettier", stop_after_first = true },
 				zig = { "zigfmt" },
